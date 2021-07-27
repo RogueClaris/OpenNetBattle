79e707943db7abe746a830e8e2927eb2ba8e9a08
Claris <seventh_sage@hotmail.com>
2021-07-27 17:30:45 -0400

    Updating font

--- BattleNetwork/resources/ow/tiles/DungeonServerWarpPads.tsx
+++ BattleNetwork/resources/ow/tiles/DungeonServerWarpPads.tsx
@@ -2,11 +2,6 @@
 <tileset version="1.5" tiledversion="1.7.1" name="DungeonServerWarpPads" tilewidth="57" tileheight="33" tilecount="60" columns="10">
  <image source="DungeonServerWarpPads.png" width="570" height="198"/>
  <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="5.70835" y="4.06248" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="0" duration="300"/>
    <frame tileid="1" duration="300"/>
@@ -16,11 +11,6 @@
   </animation>
  </tile>
  <tile id="5">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="6.04168" y="4.06248" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="5" duration="300"/>
    <frame tileid="6" duration="300"/>
@@ -30,11 +20,6 @@
   </animation>
  </tile>
  <tile id="10">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="6.04168" y="4.72915" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="10" duration="300"/>
    <frame tileid="11" duration="300"/>
@@ -44,11 +29,6 @@
   </animation>
  </tile>
  <tile id="15">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="6.37502" y="4.72915" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="15" duration="300"/>
    <frame tileid="16" duration="300"/>
@@ -58,11 +38,6 @@
   </animation>
  </tile>
  <tile id="20">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="6.70835" y="3.39582" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="20" duration="300"/>
    <frame tileid="21" duration="300"/>
@@ -72,11 +47,6 @@
   </animation>
  </tile>
  <tile id="25">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="7.37502" y="4.39582" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="25" duration="300"/>
    <frame tileid="26" duration="300"/>
@@ -86,11 +56,6 @@
   </animation>
  </tile>
  <tile id="30">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="5.37502" y="3.72915" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="30" duration="300"/>
    <frame tileid="31" duration="300"/>
@@ -100,11 +65,6 @@
   </animation>
  </tile>
  <tile id="35">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="6.70835" y="5.39582" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="35" duration="300"/>
    <frame tileid="36" duration="300"/>
@@ -114,11 +74,6 @@
   </animation>
  </tile>
  <tile id="40">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="6.37502" y="4.06248" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="40" duration="300"/>
    <frame tileid="41" duration="300"/>
@@ -128,11 +83,6 @@
   </animation>
  </tile>
  <tile id="45">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="5.04168" y="4.39582" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="45" duration="300"/>
    <frame tileid="46" duration="300"/>
@@ -142,11 +92,6 @@
   </animation>
  </tile>
  <tile id="50">
-  <objectgroup draworder="index" id="2">
-   <object id="39" x="5.37502" y="4.06248" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="50" duration="300"/>
    <frame tileid="51" duration="300"/>
@@ -156,11 +101,6 @@
   </animation>
  </tile>
  <tile id="55">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="5.04168" y="3.72915" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="55" duration="300"/>
    <frame tileid="56" duration="300"/>
--- BattleNetwork/resources/ow/tiles/SquareWarpsA.tsx
+++ BattleNetwork/resources/ow/tiles/SquareWarpsA.tsx
@@ -2,11 +2,6 @@
 <tileset version="1.5" tiledversion="1.7.1" name="SquareWarpsA" tilewidth="64" tileheight="39" tilecount="40" columns="8">
  <image source="SquareWarpsA.png" width="512" height="195"/>
  <tile id="0">
-  <objectgroup draworder="index" id="4">
-   <object id="3" x="10.75" y="7.33333" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="0" duration="1250"/>
    <frame tileid="1" duration="1250"/>
@@ -15,11 +10,6 @@
   </animation>
  </tile>
  <tile id="4">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="10.7083" y="8.39582" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="4" duration="1250"/>
    <frame tileid="5" duration="1250"/>
@@ -28,11 +18,6 @@
   </animation>
  </tile>
  <tile id="8">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="10.7083" y="7.39582" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="8" duration="1250"/>
    <frame tileid="9" duration="1250"/>
@@ -41,11 +26,6 @@
   </animation>
  </tile>
  <tile id="12">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="10.0417" y="6.39582" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="12" duration="1250"/>
    <frame tileid="13" duration="1250"/>
@@ -54,11 +34,6 @@
   </animation>
  </tile>
  <tile id="16">
-  <objectgroup draworder="index" id="2">
-   <object id="79" x="10.3333" y="7" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="16" duration="1250"/>
    <frame tileid="17" duration="1250"/>
@@ -67,11 +42,6 @@
   </animation>
  </tile>
  <tile id="20">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="11.0417" y="8.72915" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="20" duration="1250"/>
    <frame tileid="21" duration="1250"/>
@@ -80,11 +50,6 @@
   </animation>
  </tile>
  <tile id="24">
-  <objectgroup draworder="index" id="2">
-   <object id="34" x="10.375" y="6.39582" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="24" duration="1250"/>
    <frame tileid="25" duration="1250"/>
@@ -93,11 +58,6 @@
   </animation>
  </tile>
  <tile id="28">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="11.7083" y="7.72915" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="28" duration="1250"/>
    <frame tileid="29" duration="1250"/>
@@ -106,11 +66,6 @@
   </animation>
  </tile>
  <tile id="32">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="11.0417" y="7.39582" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="32" duration="1250"/>
    <frame tileid="33" duration="1250"/>
@@ -119,11 +74,6 @@
   </animation>
  </tile>
  <tile id="36">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="10.375" y="7.06248" width="42.5833" height="22.5417">
-    <ellipse/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="36" duration="1250"/>
    <frame tileid="37" duration="1250"/>
